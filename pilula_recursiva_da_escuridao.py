5f6da533b27bf909dbde0efe0bf1f85d51e31c27
Samuel Sobreira <samuel.sobreira@camara.leg.br>
2024-10-18 17:52:51 -0300

    o primeiro

--- pilula_recursiva_da_escuridao.py
+++ pilula_recursiva_da_escuridao.py
@@ -1,18 +1,11 @@
 import os
 import time
 import random
+import threading
 # 30 x 8 = 240
 
-matriz = f"""
-    --------------------------
-    |     A B C D E F G      |
-    |     H I J K L M N      |  
-    |     O P Q R S T U      |
-    |     V X Y Z R R R      | 
-    |     R R R R R R R      |
-    |     R R R R R R R      | 
-    --------------------------
-"""
+matriz = "---------------------------\n|     A B C D E F G       |\n|     H I J K L M N       |\n|     O P Q R S T U       |\n|     V X Y Z R R R       |\n|     R R R R R R R       |\n|     R R R R R R R       |\n---------------------------\n"
+
 
 def dark_painting(pos,matriz):
     if i == None:
@@ -22,28 +15,30 @@ def dark_painting(pos,matriz):
 
 
 def pilula_recursiva(i,matriz,aux):
-    if matriz[i] == "*":
-        i = random.randint(1,240)
+    if matriz[i] == "*" or matriz[i] == "-" or matriz[i] == "|" or matriz[i-1] == "|" or matriz[i-1] == "-":
+        i = random.randint(1,len(matriz)-1)
 
         if aux < 100:
             print(aux)
             pilula_recursiva(i,matriz,aux+1)
-            
         else:
             return 666
     else:
         return i
 
 
+seconds = 0
 darkness = False
 while (not darkness):
-    i = random.randint(1,240)
+    #print(f"Time: {seconds}s")
+    i = random.randint(1,len(matriz)-1)
     i = pilula_recursiva(i,matriz,1)
     if i != 666:
         os.system("cls")
         matriz = dark_painting(i,matriz)
         print(matriz)
-        time.sleep(0.1)
+        time.sleep(0.07)
+        seconds += 1
     else:
         darkness = True
     
